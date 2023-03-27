6743f443609cb3008741ec2c32f037ce659a1cc0
Fernando Fox <fernandodiego.sn@gmail.com>
2023-03-27 17:10:48 -0300

    Configurando o repository e refatorando retirando os construtores estáticos e carregando lista inicial diretamente do banco

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,24 +1,26 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     @ResponseBody
     public List<TopicoDTO> lista() {
-        Topico topico = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-
-        return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDTO.converter(topicos);
     }
 
 }
