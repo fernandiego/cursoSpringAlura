5766cff8b8fa54037dfc6f7aca74dc7e6c493a5c
Fernando Fox <fernandodiego.sn@gmail.com>
2023-05-02 19:30:52 -0300

    implementando o update de tópicos

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -2,12 +2,14 @@ package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDTO;
 import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -49,6 +51,13 @@ public class TopicosController {
     public DetalhesDoTopicoDTO detalhar(@PathVariable Long id) {
         Topico topico = topicoRepository.getOne(id);
         return new DetalhesDoTopicoDTO(topico);
+    }
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+        Topico topico = form.atualizar(id, topicoRepository);
+
+        return ResponseEntity.ok(new TopicoDTO(topico));
     }
 }
 
---
+++ forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,45 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.hibernate.validator.constraints.Length;
+import org.jetbrains.annotations.NotNull;
+import javax.validation.constraints.NotEmpty;
+
+public class AtualizacaoTopicoForm {
+    @NotNull
+    @NotEmpty
+    @Length(min = 5)
+    private String titulo;
+    @NotNull
+    @NotEmpty
+    @Length(min = 10)
+    private String mensagem;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+
+        Topico topico = topicoRepository.getOne(id);
+        topico.setTitulo(this.titulo);
+        topico.setMensagem(this.mensagem);
+
+        return topico;
+    }
+}
